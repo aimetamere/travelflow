1e6b9bd73b536c4b137ffb2af82b6eb5ec03e5d9
aimetamere <nicolas.dehaan@icloud.com>
2025-04-26 10:24:05 +0200

    room with liveblocks

--- components/room.tsx
+++ components/room.tsx
@@ -1,6 +1,24 @@
 "use client";
 
 import { ReactNode } from "react";
-import { ClientSideSuspense } from "@liveblocks/react";
+import { ClientSideSuspense } from "@liveblocks/react"; // npm package  
 
-import { RoomProvider } from "@/liveblocks.config";
+import { RoomProvider } from "@/liveblocks.config"; // alias to file 
+
+interface RoomProps {
+    children: ReactNode;
+    roomId: string;
+};
+
+export const Room = ({
+    children,
+    roomId,
+}: RoomProps) => {
+    return ( // initial presence is empty object
+        <RoomProvider id={roomId} initialPresence={{}}> 
+            <ClientSideSuspense fallback={<div>Loading...</div>}>
+                {() => children}
+            </ClientSideSuspense>
+        </RoomProvider>
+    );
+}; 
