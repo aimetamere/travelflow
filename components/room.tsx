81ce99dbefc43935c9dbfcb69026d4e4b364a5ad
aimetamere <nicolas.dehaan@icloud.com>
2025-04-27 22:37:23 +0200

    loading added not done yet

--- components/room.tsx
+++ components/room.tsx
@@ -8,15 +8,18 @@ import { RoomProvider } from "@/liveblocks.config"; // alias to file
 interface RoomProps {
     children: ReactNode;
     roomId: string;
+    fallback: NonNullable<ReactNode> | null;
+    // fallback: NonNullable<ReactNode> | null; // for suspense
 };
 
 export const Room = ({
     children,
     roomId,
+    fallback,
 }: RoomProps) => {
     return ( // initial presence is empty object
         <RoomProvider id={roomId} initialPresence={{}}> 
-            <ClientSideSuspense fallback={<div>Loading...</div>}>
+            <ClientSideSuspense fallback={fallback}>
                 {() => children}
             </ClientSideSuspense>
         </RoomProvider>
