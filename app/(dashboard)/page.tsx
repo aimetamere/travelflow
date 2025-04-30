cf1aad0664940165ab8a174ef9c6ac949e5f9b64
aimetamere <nicolas.dehaan@icloud.com>
2025-04-30 15:45:30 +0200

    test for deployment

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,35 +1,30 @@
 "use client";
- 
- import { useOrganization } from "@clerk/nextjs";
 
+import { useSearchParams } from "next/navigation";
+import { useOrganization } from "@clerk/nextjs";
 
 import { EmptyOrg } from "./_components/empty-org";
 import { BoardList } from "./_components/board-list";
 
-interface DashboardPageProps {
-    searchParams: {
-        search?: string;
-        favorites?: string;
-    };
+const DashboardPage = () => {
+  const { organization } = useOrganization();
+  const searchParams = useSearchParams();
+
+  const search = searchParams.get("search") || undefined;
+  const favorites = searchParams.get("favorites") || undefined;
+
+  return (
+    <div className="flex-1 h-[calc(100%-80px)] p-6">
+      {organization ? (
+        <BoardList
+          orgId={organization.id}
+          query={{ search, favorites }}
+        />
+      ) : (
+        <EmptyOrg />
+      )}
+    </div>
+  );
 };
-
-const DashboardPage = ({
-    searchParams,
-}: DashboardPageProps) => {
-    const { organization } = useOrganization();
-
-    return (
-        <div className="flex-1 h-[calc(100%-80px)] p-6">
-            {organization ? (
-                <BoardList
-                    orgId={organization.id}
-                    query={searchParams}
-                />
-            ) : (
-                <EmptyOrg />
-            )}
-        </div>
-    );
-}
 
 export default DashboardPage;
